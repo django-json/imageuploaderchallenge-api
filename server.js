b103dc571eb554869277623a41afc8ed36fb0cc4
django-json <diango.jayson@gmail.com>
2022-01-20 13:15:59 +0800

    implement gzipping and basic security

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const compression = require('compression'); 
+const helmet = require('helmet');
 
 const {multerUploads, dataUri} = require('./middlewares/multer');
 const { cloudinaryConfig, uploader } = require('./middlewares/cloudinary'); 
@@ -11,6 +13,8 @@ const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(cors());
+app.use(compression());
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 app.use(cloudinaryConfig);
@@ -21,7 +25,7 @@ app.get('/', (req, res) => {
 });
 
 app.post(
-    '/upload-image', 
+    '/uploads', 
     multerUploads, 
     upload.uploadImage(uploader, dataUri)
 );
